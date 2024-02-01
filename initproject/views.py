eaf6b04980ab91d7992b211396575f0be4563619
upendraupi <upendrakuruma@gmail.com>
2024-02-01 13:02:37 +0530

    first commit

---
+++ initproject/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def first(request):
+    return HttpResponse ('this is a pratice project')
+def second(request):
+    return HttpResponse ('this is a pratice project')
